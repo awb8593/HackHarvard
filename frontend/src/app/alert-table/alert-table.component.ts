039df16e8c2f4375c65c7e4097f5b6c5c71b6487
Adrian Burgos <awb8593@rit.edu>
2023-10-21 11:48:00 -0400

    Inserted alert-table into alert-page

---
+++ frontend/src/app/alert-table/alert-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-table',
+  templateUrl: './alert-table.component.html',
+  styleUrls: ['./alert-table.component.css']
+})
+export class AlertTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
