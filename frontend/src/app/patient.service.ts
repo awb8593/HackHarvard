1b6f78e1ba2f535b55b21d73f6df0ed6fda6ab5d
Adrian Burgos <awb8593@rit.edu>
2023-10-22 04:42:11 -0400

    Populate mock data with api data

--- frontend/src/app/patient.service.ts
+++ frontend/src/app/patient.service.ts
@@ -11,13 +11,20 @@ export class PatientService {
 
   constructor(private rootService: RootService) {}
 
-  createMockPatient(name: string, id: number): Promise<Patient> {
+  createMockPatient(name: string, id: number): Patient {
+    return new Patient(id, name, 5857481591, new Array<Provider>());
+
+  }
+  
+  createMockPatientAlert(patient: Patient): Promise<PatientAlert> {
     return new Promise((resolve, reject) => {
       this.rootService.getPatientAlert().subscribe(
         (data: any) => {
           console.log("THE API HAS RETURNED:", data);
-          const patient = new Patient(id, name, 5857481591, new Array<Provider>());
-          resolve(patient);
+          const alertContent = JSON.parse(data[0].message.content);
+
+          const alert = new PatientAlert(alertContent.priority, patient, alertContent.detailedNotes + alertContent.assessment + alertContent.actionItems + alertContent.followUp);
+          resolve(alert);
         },
         (error: any) => {
           console.error("Error while fetching data:", error);
--- frontend/src/app/provider.service.ts
+++ frontend/src/app/provider.service.ts
@@ -3,19 +3,24 @@ import { Provider } from './models/provider.model';
 import { Patient } from './models/patient.model';
 import { PatientAlert } from './models/patient-alert.model';
 import { PatientService } from './patient.service';
+import { RootService } from './root/root.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderService {
 
-  constructor(private patientService: PatientService) {}
+  constructor(private patientService: PatientService, private rootService: RootService) {}
 
   async createMockProvider() {
 
-    const patient1 = await this.patientService.createMockPatient("Onvida Serixay", 1);
-    const patient2 = await this.patientService.createMockPatient("Kasim", 5);
-    const patient3 = await this.patientService.createMockPatient("Diego", 10);
+    const patient1 = new Patient(1, "Onvida", 5857481591, new Array<Provider>)
+    const patient2 = new Patient(7, "Diego", 5857481591, new Array<Provider>)
+    const patient3 = new Patient(10, "Kasim", 5857481591, new Array<Provider>)
+    const patientAlert1 = await this.patientService.createMockPatientAlert(patient1);
+    const patientAlert2 = await this.patientService.createMockPatientAlert(patient2);
+    const patientAlert3 = await this.patientService.createMockPatientAlert(patient3);
+
 
     return new Provider(
       1, 
@@ -26,9 +31,9 @@ export class ProviderService {
         patient1, patient2, patient3
       ),
       new Array<PatientAlert>(
-        new PatientAlert(5, new Patient(1, "Onvida", 5857481591, new Array<Provider>), "ALERT! Immediate attention may be needed!"),
-        new PatientAlert(2, new Patient(7, "Diego", 5857481591, new Array<Provider>), "Starting to show minor symptoms."),
-        new PatientAlert(1, new Patient(10, "Kasim", 5857481591, new Array<Provider>), "Healthiest guy I've ever seen")
+        patientAlert1,
+        patientAlert2,
+        patientAlert3
       )
     )
   }
